79350405f5c9013236ab405bc59bfadc78a25901
ACullen97 <alex_cullen123@hotmail.co.uk>
2025-02-22 10:32:32 +0000

    Updated display issues

--- src/components/My404Component.jsx
+++ src/components/My404Component.jsx
@@ -1,6 +1,6 @@
 const My404Component = () => {
     return (
-      <div>
+      <div className="error-msg">
         <h2>404: Not Found</h2>
         <p>The page you are looking for doesn't exist!</p>
       </div>
--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
-import  { getArticles } from "../api";
-import ArticleVoteCounter from "../components/ArticleVoteCounter"
+import { getArticles } from "../api";
+import ArticleVoteCounter from "../components/ArticleVoteCounter";
 
-function SingleArticleCard({article}){
+function SingleArticleCard({ article }) {
+  console.log({ article });
 
-
-
-    return(
-        <>
-        <h2 className="all-articles-header">View Article</h2>
-        <div className="item-card-static">
-          <div className="item-card-info">
-            <h2>{article.title}</h2>
-            <br />
-            <h3>Topic: {article.topic}</h3>
-            <br />
-            <p>Topic: {article.body}</p>
-            <br />
-            <h4>Author: {article.author}</h4>
-            <br/> 
-            <ArticleVoteCounter articleVotes={article.votes}/>
+  return (
+    <>
+      <h2 className="all-articles-header">View Article</h2>
+      <div className="item-card-static">
+        <div className="item-card-info">
+          <h2>{article.title}</h2>
+          <br />
+          <h3>Topic: {article.topic}</h3>
+          <br />
+  
+          <p>Topic: {article.body}</p>
+          <br/>
+          <div className="img-container">
+           <img src={article.article_img_url}></img>
           </div>
-    
+
+          <h4>Author: {article.author}</h4>
+          <br />
+          <ArticleVoteCounter articleVotes={article.votes} />
         </div>
-     
-        </>
-    )
+      </div>
+    </>
+  );
 }
 export default SingleArticleCard;
